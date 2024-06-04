5b02d5002a65efa96f97933f2112c6a122290edd
Fuuulkrum7 <ilka747428@gmail.com>
2024-06-04 15:51:11 +0300

    Add basic work with xml

---
+++ DefaultSettings.h
@@ -0,0 +1,16 @@
+#ifndef DEFAULTSETTINGS_H
+#define DEFAULTSETTINGS_H
+
+#include <QString>
+
+static const QString SETTINGS_NAME = ".settings.xml";
+static const QString SETTINGS_FOLDER = ".settings";
+
+static const QString START_ELEMENT = "settings";
+static const QString LOW_BORDER = "low_border";
+static const QString HIGH_BORDER = "high_border";
+
+static const float LOW_BORDER_VALUE = 0;
+static const float HIGH_BORDER_VALUE = 100;
+
+#endif // DEFAULTSETTINGS_H
--- LightMonitoring.pro
+++ LightMonitoring.pro
@@ -1,4 +1,5 @@
 QT       += core gui
+QT       += xml
 
 greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
 
@@ -10,10 +11,13 @@ CONFIG += c++17
 
 SOURCES += \
     main.cpp \
-    mainwindow.cpp
+    mainwindow.cpp \
+    settings.cpp
 
 HEADERS += \
-    mainwindow.h
+    DefaultSettings.h \
+    mainwindow.h \
+    settings.h
 
 FORMS += \
     mainwindow.ui
