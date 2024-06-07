cdfb51f4f2b120ea43c29f9c38c975f64bb18867
Vanillla-Ice <krasavin.danila@yandex.ru>
2024-06-07 22:14:38 +0300

    Added relative path for translator

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,6 +2,7 @@
 #include "ui_mainwindow.h"
 #include <QDebug>
 #include <QApplication>
+#include <QDir>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -18,7 +19,14 @@ MainWindow::~MainWindow()
 
 void MainWindow::changeLanguage(const QString &languageCode) {
     QCoreApplication::removeTranslator(&translator);
-    if (translator.load("C:\\lighttm\\LightMonitoring\\translations\\LightMonitoring_" + languageCode + ".qm")) {
+    QString currentPath = QDir::currentPath();
+    QString targetFolder = "LightMonitoring";
+    QString resultPath;
+
+    int targetIndex = currentPath.indexOf(targetFolder);
+    if (targetIndex != -1)
+        resultPath = currentPath.left(targetIndex + targetFolder.length());
+    if (translator.load(resultPath + "/translations/LightMonitoring_" + languageCode + ".qm")) {
         qDebug() << "success";
         QCoreApplication::installTranslator(&translator);
         ui->retranslateUi(this);  // Обновить UI для перевода
